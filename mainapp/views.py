73918f563ca5e19729bfd640e3e7da08d5df6646
dark-rose-ctrl <kudasovaroza@gmail.com>
2025-01-05 18:52:01 +0500

    first commit

---
+++ mainapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import ToDo
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def todo_list(request):
+    todos = ToDo.objects.all()
+    context = {'todos': todos}
+
+    return render(request, 'todo_list.html', context = context)
